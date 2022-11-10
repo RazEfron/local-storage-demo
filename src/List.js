b5ea33745d03e7745e2a382560a0cbeaa2378ab4
jdrichards-pursuit <jdrpursuit@gmail.com>
2022-11-10 08:47:43 -0500

    add changes in List

---
+++ src/List.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+/**
+ * Goal is to be able to choose a category,
+ * Receive the data from the api the first time,
+ * store it in localStorage
+ * list out the names or titles
+ * if the data has been called before then do not make an api call
+ * instead, retrieve the data from localStorage
+ *
+ */
+
+export default function List() {
+  const [category, setCategory] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = `https://ghibliapi.herokuapp.com/`;
+  });
+
+  return (
+    <div className="list">
+      <h2>Choose A Category</h2>
+      <select>
+        <option value=""></option>
+        <option value="people">People</option>
+        <option value="locations">Locations</option>
+        <option value="films">Films</option>
+      </select>
+    </div>
+  );
+}
