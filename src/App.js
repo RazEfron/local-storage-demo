109fc37aede96a5c2437d3e64fe2b9cae894ce4a
jdrichards-pursuit <jdrpursuit@gmail.com>
2022-11-10 08:46:10 -0500

    set goals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import List from "./List";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Pursuit App Template</h1>
+      <List />
     </div>
   );
 }
