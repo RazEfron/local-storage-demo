ed6cae022b165ce5de777c0d710c89615ad36d16
RazEfron <razefron01@gmail.com>
2022-11-16 20:55:40 -0500

    add thoughts about comments

--- src/List.js
+++ src/List.js
@@ -7,7 +7,6 @@ import { useState, useEffect } from "react";
  * list out the names or titles
  * if the data has been called before then do not make an api call
  * instead, retrieve the data from localStorage
- *
  */
 
 export default function List() {
@@ -16,26 +15,34 @@ export default function List() {
 
   // this state will be used to teach basics of localStorage. Not needed for app.
   // const [name, setName] = useState("");
-    // const [name, setName] = useState(
-    //   JSON.parse(window.localStorage.getItem("myName")) || ""
-    // );
+  // const [name, setName] = useState(
+  //   JSON.parse(window.localStorage.getItem("myName")) || "" // localStorage["myName"]
+  // );
 
   useEffect(() => {
     // console.log("name", name);
-    //BASIC STORAGE
+    // BASIC STORAGE
     // const myName = { first: "JD", last: "Richards" };
+    // window.localStorage.setItem("myName", JSON.stringify(myName)); // localStorage["myName"] = JSON.stringify(myName)
 
-    // window.localStorage.setItem("myName", JSON.stringify(myName));
     const url = `https://ghibliapi.herokuapp.com/${category}`;
+    const result = JSON.parse(window.localStorage.getItem(category));
+    // We check if we have a key of the category already in local host
 
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((res) => {
-        console.log(`I ran a fetch for ${category}`);
-        // I want to set my state with the data
-        setData(res);
-        // I want to reset the category to prevent an infinite loop
-      });
+    if (result) {
+      // If we have a the data saved in localhost we don't make a fetch request
+      setData(result);
+    } else {
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((res) => {
+          console.log(`I ran a fetch for ${category}`);
+          // We save the data from the fetch request on localhost so that next time we choose that category it goes to localhost and not to fetch
+          window.localStorage.setItem(category, JSON.stringify(res));
+          // I want to set my state with the data
+          setData(res);
+        });
+    }
   }, [category]);
 
   return (
@@ -52,3 +59,26 @@ export default function List() {
     </div>
   );
 }
+// Thoughts about saving comments in localStorage
+// function Video() {
+//   const { id } = useParams();
+//   const [comments, setComments] = useState(
+//     JSON.parse(window.localStorage.getItem(id)) || []
+//   );
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     const newComments = [...comments, "commentFromForm"];
+//     window.localStorage.setItem(id, JSON.stringify(newComments));
+//     setComments(newComments);
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}></form>
+//       {comments.map((comment) => (
+//         <li>{comment}</li>
+//       ))}
+//     </div>
+//   );
+// }
